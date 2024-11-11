8ae950a0270eebb6f979f42529fdda5b278f0719
Justin Myers <jmyers.sde@gmail.com>
2024-11-11 00:19:52 -0500

    I have the code done but I do not know whay it doesn't work. Need help.

--- src/MovieNightPlanner.js
+++ src/MovieNightPlanner.js
@@ -1,27 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, } from 'react';
 
 function MovieNightPlanner() {
   // TODO: Initialize state for movies, newMovie, and selectedMovie
   // Hint: Use the useState hook
+  const [movies, setMovies] = useState([]);
+  const [newMovie, setNewMovie] = useState('');
+  const [selectedMovie, setSelectedMovie] = useState('');
+
   
   // TODO: Implement useEffect to load movies from localStorage
   useEffect(() => {
     // Add your code here
+    const storedMovies = localStorage.getItem('movies');
+    if(storedMovies){
+      setMovies(JSON.parse(storedMovies))
+    }
   }, []);
 
   // TODO: Implement useEffect to save movies to localStorage
   useEffect(() => {
     // Add your code here
-  }, [/* TODO: Add dependency */]);
+    localStorage.setItem('movies', JSON.stringify(movies))
+  }, [movies]);
 
   const addMovie = () => {
     // TODO: Implement addMovie function
     // Hint: Check if newMovie is not empty, add it to movies, and clear the input
+    if(newMovie.trim()){
+      setMovies([...movies, newMovie]);
+      setNewMovie('');
+    }
   };
 
   const pickMovie = () => {
     // TODO: Implement pickMovie function
     // Hint: Randomly select a movie from the movies array
+    const randomIndex = Math.floor(Math.random() * movies.length);
+    setSelectedMovie(movies[randomIndex]);
   };
 
   return (
@@ -31,13 +46,17 @@ function MovieNightPlanner() {
         type="text"
         placeholder="Add new movie"
         // TODO: Add value and onChange props
+        value={newMovie}
+        onChange={(e) => setNewMovie(e.target.value)}
       />
       <button onClick={addMovie}>Add Movie</button>
       <button onClick={pickMovie}>Pick a Movie</button>
-      <h2>Selected Movie: {/* TODO: Display selectedMovie */}</h2>
+      <h2>Selected Movie: {{selectedMovie}}</h2>
       <h3>Watchlist:</h3>
       <ul>
-        {/* TODO: Render list of movies */}
+        {movies.map((movie, index) => (
+          <li key={index}>{movie}</li>
+        ))}
         {/* Hint: Use movies.map() to render each movie */}
       </ul>
     </div>
--- src/MovieNightPlanner.test.js
+++ src/MovieNightPlanner.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import MovieNightPlanner from './MovieNightPlanner';
+import MovieNightPlanner from '../src/MovieNightPlanner';
 
 describe('MovieNightPlanner', () => {
   beforeEach(() => {
